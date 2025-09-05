16b8857dea370888de2d1050291dc0b875847c5e
JTHeracing <jth@e-racing.world>
2025-09-05 13:51:23 +0200

    Update dataSlave.py

--- dataSlave.py
+++ dataSlave.py
@@ -13,16 +13,22 @@ def on_connect(client, userdata, flags, rc):
     else:
         print("Forbindelse fejlet rc: ", rc)
 
-
+def on_logIn(client, userdata, level, buf):
+    print("log: ", buf);
 
 def on_message(client, userdata, msg):
     print(f"Modtaget besked på {msg.topic}: {msg.payload.decode()}")
 
 client.on_connect = on_connect
+client.on_log = on_logIn
 client.on_message = on_message
 
 broker = "4d881b47b776416f9fe8b1df9fe3ae48.s1.eu.hivemq.cloud"
 port = 8883
+
+client.username_pw_set("dataUpload", "dataUpload123")
+
+client.tls_set()
 
 client.connect(broker, port)
 
